4d4d74a50b870e36f8433e3a662e6c6693471b00
Geth-Ghosh-2004 <ghoshjeet455@gmail.com>
2025-10-18 14:39:29 +0530

    Update generateTokenAndSetCookie.js

--- server/utils/generateTokenAndSetCookie.js
+++ server/utils/generateTokenAndSetCookie.js
@@ -7,8 +7,8 @@ export const generateTokenAndSetCookie = (res, userId) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV-- - "production",
-    satmeSite: "strict",
+    secure: process.env.NODE_ENV --- "production",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
