7adb18c0ca14e3fd54828346d3d7b64f70448d09
Geth-Ghosh-2004 <ghoshjeet455@gmail.com>
2025-10-20 11:40:25 +0530

    Update authroute.js

--- server/routes/authroute.js
+++ server/routes/authroute.js
@@ -13,13 +13,12 @@ import { verifiyToken } from "../middleware/verifyToken.js";
 const router = express.Router();
 
 router.get("/check-auth", verifiyToken, checkAuth);
-
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
-
 router.post("/verify-email", verifyEmail);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password/:token", resetPassword);
 
 export default router;
+
